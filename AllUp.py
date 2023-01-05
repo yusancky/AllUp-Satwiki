b523ee4958d4c82d735e5b2b299458bcc7490aab
yusancky <umbrellacky@qq.com>
2023-01-05 10:31:24 +0800

    Use Installed Browsers and Drivers

--- AllUp.py
+++ AllUp.py
@@ -8,17 +8,16 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from time import localtime,strftime
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
 
-chrome_service = Service(ChromeDriverManager(chrome_type = ChromeType.CHROMIUM).install())
+chrome_service = Service(executable_path = "/usr/local/share/chrome_driver")
 chrome_options = Options()
 for option in ["--headless","--disable-gpu","--window-size=1920,1200","--ignore-certificate-errors","--disable-extensions","--no-sandbox","--disable-dev-shm-usage"]:
     chrome_options.add_argument(option)
-driver = webdriver.Chrome(service = chrome_service,options = chrome_options)
+chrome_options.binary_location = "/usr/bin/google-chrome"
+chrome = webdriver.Chrome(service = chrome_service,options = chrome_options)
 
-driver.get("https://voyager.jpl.nasa.gov/mission/status/")
-page_source = driver.page_source
+chrome.get("https://voyager.jpl.nasa.gov/mission/status/")
+page_source = chrome.page_source
 
 sections = ["km","au","kms","aus","speed","lt"]
 AllUp_content = "<includeonly>{{ #switch: {{{1|}}} | 1={{ #switch: {{{id}}} | t = time | 1 = {{ #switch: {{{section}}} | km = data1 | au = data2 | kms = data3 | aus = data4 | speed = data5 | lt = data6 | #default = 请输入正确的选项名！}} | 2 = {{ #switch: {{{section}}} | km = data7 | au = data8 | kms = data9 | aus = data10 | speed = data11 | lt = data12 | #default = 请输入正确的选项名！}} | #default = 请输入正确的编号！ }} | #default = 请输入正确的 AllUp 编号！}}</includeonly><noinclude>[[Category:模板]]{{documentation}}</noinclude>".replace("time",strftime("%Y年%m月%d日%H时",localtime()))
