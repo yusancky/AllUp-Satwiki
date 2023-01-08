2a678b39a498fec537a3fe807e36f04f72ab4593
yusancky <umbrellacky@qq.com>
2023-01-08 20:49:24 +0800

    Fix Another New Issue

--- AllUp.py
+++ AllUp.py
@@ -21,14 +21,14 @@ driver.get("https://voyager.jpl.nasa.gov/mission/status/")
 page_source = driver.page_source
 
 sections = ["km","au","kms","aus","speed","lt"]
-AllUp_content = "<includeonly>{{#switch:{{{1|}}}|1={{#switch:{{{id}}}|t=time|1={{#switch:{{{section}}}|km=data1|au=data2|kms=data3|aus=data4|speed=data5|lt=data6|#default=请输入正确的选项名！}}|2={{#switch:{{{section}}}|km=data7|au=data8|kms=data9|aus=data10|speed=data11|lt=data12|#default=请输入正确的选项名！}}|#default=请输入正确的编号！}}|#default=请输入正确的AllUp编号！}}</includeonly><noinclude>[[Category:模板]]{{documentation}}</noinclude>".replace("time",strftime("%Y 年 %m 月 %d 日 %H 时",localtime()))
+AllUp_content = "<includeonly>{{#switch:{{{1|}}}|1={{#switch:{{{id}}}|t=time|1={{#switch:{{{section}}}|km=data1 |au=data2 |kms=data3 |aus=data4 |speed=data5 |lt=data6 |#default=请输入正确的选项名！}}|2={{#switch:{{{section}}}|km=data7 |au=data8 |kms=data9 |aus=data10 |speed=data11 |lt=data12 |#default=请输入正确的选项名！}}|#default=请输入正确的编号！}}|#default=请输入正确的AllUp编号！}}</includeonly><noinclude>[[Category:模板]]{{documentation}}</noinclude>".replace("time",strftime("%Y 年 %m 月 %d 日 %H 时",localtime()))
 cnt = 0
 for id in [1,2]:
     for section in sections:
         cnt += 1
         data_re = compile(f'id="voy{id}_{section}">(.*)</div>')
         findall_res = findall(data_re,page_source)
-        AllUp_content = AllUp_content.replace(f"data{cnt}",f"{findall_res[0]}")
+        AllUp_content = AllUp_content.replace(f"data{cnt} ",f"{findall_res[0]}")
 
 wiki = Wiki("sat.huijiwiki.com","雨伞CKY",environ["SATWIKI_PASSWORD"])
 wiki.edit("模板:AllUp",AllUp_content,"Edit via AllUp-Satwiki")
