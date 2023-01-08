49e10139b26e25cdefa3b141050a39c4404ffff0
yusancky <umbrellacky@qq.com>
2023-01-08 20:38:44 +0800

    Use Smaller Wikitext

--- AllUp.py
+++ AllUp.py
@@ -21,7 +21,7 @@ driver.get("https://voyager.jpl.nasa.gov/mission/status/")
 page_source = driver.page_source
 
 sections = ["km","au","kms","aus","speed","lt"]
-AllUp_content = "<includeonly>{{ #switch: {{{1|}}} | 1={{ #switch: {{{id}}} | t = time | 1 = {{ #switch: {{{section}}} | km = data1 | au = data2 | kms = data3 | aus = data4 | speed = data5 | lt = data6 | #default = 请输入正确的选项名！}} | 2 = {{ #switch: {{{section}}} | km = data7 | au = data8 | kms = data9 | aus = data10 | speed = data11 | lt = data12 | #default = 请输入正确的选项名！}} | #default = 请输入正确的编号！ }} | #default = 请输入正确的 AllUp 编号！}}</includeonly><noinclude>[[Category:模板]]{{documentation}}</noinclude>".replace("time",strftime("%Y年%m月%d日%H时",localtime()))
+AllUp_content = "<includeonly>{{#switch:{{{1|}}}|1={{#switch:{{{id}}}|t=time|1={{#switch:{{{section}}}|km=data1|au=data2|kms=data3|aus=data4|speed=data5|lt=data6|#default=请输入正确的选项名！}}|2={{#switch:{{{section}}}|km=data7|au=data8|kms=data9|aus=data10|speed=data11|lt=data12|#default=请输入正确的选项名！}}|#default=请输入正确的编号！}}|#default=请输入正确的AllUp编号！}}</includeonly><noinclude>[[Category:模板]]{{documentation}}</noinclude>".replace("time",strftime("%Y 年 %m 月 %d 日 %H 时",localtime()))
 cnt = 0
 for id in [1,2]:
     for section in sections:
