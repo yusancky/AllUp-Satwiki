d71a354241992edeea959a1c67699f58b9b87fa7
yusancky <yusancky@hotmail.com>
2023-02-05 03:32:02 +0800

    fix: folder name in content

--- generate.py
+++ generate.py
@@ -12,7 +12,7 @@ def make(id):
         case 't':
             return strftime('%Y年%m月%d日%H时',localtime())
         case '1':
-            web_data = AllUp-utils.web.fetch_data(
+            web_data = AllUp_utils.web.fetch_data(
                 'https://voyager.jpl.nasa.gov/mission/status/',
                 chromedriver
             )
@@ -26,22 +26,22 @@ def make(id):
                     )[0]
                 data[id]['#default'] = '请输入正确的选项名！'
             data['#default'] = '请输入正确的编号！'
-            return AllUp-utils.wikitext.switch(data)
+            return AllUp_utils.wikitext.switch(data)
         case '2':
-            web_data = AllUp-utils.web.fetch_data('https://in-the-sky.org/spacecraft.php?id=6073')
+            web_data = AllUp_utils.web.fetch_data('https://in-the-sky.org/spacecraft.php?id=6073')
             data = {'switch_key': 'section'}
             for section in ['Inclination','Eccentricity','RA ascending node','Argument perihelion','Mean anomaly','Orbital period','Epoch of osculation']:
                 data[section] = findall(f'<td>{section}</td>\n *<td>([^<>]*)</td>',web_data)[0].strip()
             data['#default'] = '请输入正确的选项名！'
-            return AllUp-utils.wikitext.switch(data)
+            return AllUp_utils.wikitext.switch(data)
         case _:
             return '请输入正确的AllUp编号！'
 
 if __name__ == '__main__':
-    chromedriver = AllUp-utils.web.configure_chromedriver()
+    chromedriver = AllUp_utils.web.configure_chromedriver()
     AllUp_data = {'switch_key': '1'}
     for dataset in ['t'] + [str(i + 1) for i in range(2)] + ['#default']:
         AllUp_data[dataset] = make(dataset)
-    AllUp_content = f"<includeonly>{AllUp-utils.wikitext.switch(AllUp_data)}</includeonly><noinclude>[[Category:模板]]{{{{documentation}}}}</noinclude>"
+    AllUp_content = f"<includeonly>{AllUp_utils.wikitext.switch(AllUp_data)}</includeonly><noinclude>[[Category:模板]]{{{{documentation}}}}</noinclude>"
     system(f'echo "ALLUP_CONTENT={AllUp_content}" >> $GITHUB_OUTPUT')
     print(f'==== AllUp_Content ====\n{AllUp_content}')
