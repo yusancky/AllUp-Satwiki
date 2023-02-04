382b5a044d159a970d8fa999dd82f99c884a485d
yusancky <yusancky@hotmail.com>
2023-02-04 19:48:24 +0000

    refactor: Use folder  (#21)

---
+++ AllUp_utils/web.py
@@ -0,0 +1,22 @@
+# Copyright (c) yusancky. All rights reserved. 
+# Licensed under the Apache License 2.0. See License in the project root for license information. 
+
+from requests import get
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+def configure_chromedriver():
+    chrome_service = Service('/usr/local/share/chrome_driver/chromedriver')
+    chrome_options = Options()
+    for option in ['--headless','--disable-gpu','--window-size=1920,1200','--ignore-certificate-errors','--disable-extensions','--no-sandbox','--disable-dev-shm-usage']:
+        chrome_options.add_argument(option)
+    return webdriver.Chrome(service = chrome_service,options = chrome_options)
+
+def fetch_data(url,need_selenium = False):
+    if need_selenium:
+        need_selenium.get(url)
+        return need_selenium.page_source
+    else:
+        response = get(url)
+        return response.text
