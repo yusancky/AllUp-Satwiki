bc81248e3d5e536268149fe68760a56b93652b9a
yusancky <umbrellacky@qq.com>
2023-08-26 17:49:25 +0800

    fix for PR preview

--- AllUp_utils/push.py
+++ AllUp_utils/push.py
@@ -1,11 +1,15 @@
 # Copyright (c) yusancky. All rights reserved. 
 # Licensed under the Apache License 2.0. See License in the project root for license information. 
 
-from os import environ,system
+from os import environ
 from pwiki.wiki import Wiki
 
 def push(title : str,content_id : str,content : str):
     try:
+        print(
+            f'### {content_id}\n\n```go\n{content}\n```\n\n',
+            file = open('../PR_preview.md','a')
+        )
         system(f'echo "{content_id}_CONTENT={content}" >> $GITHUB_OUTPUT')
         if environ['GITHUB_REF'] == 'refs/heads/main' and environ['GITHUB_REPOSITORY_OWNER'] == 'yusancky':
             wiki = Wiki('sat.huijiwiki.com','雨伞CKY',environ['SATWIKI_PASSWORD'])
