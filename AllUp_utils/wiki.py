1274aa02dc3cc953477484e197dbfab3931f6cf0
yusancky <yusancky@hotmail.com>
2023-10-15 02:35:18 +0800

    fix: Use local data file to test `TSS.py` for PR (#61)

--- AllUp_utils/wiki.py
+++ AllUp_utils/wiki.py
@@ -4,30 +4,45 @@
 from os import environ
 from pwiki.wiki import Wiki
 
-def pull(title : str):
-    try:
-        if environ['GITHUB_REF'] == 'refs/heads/main' and environ['GITHUB_REPOSITORY_OWNER'] == 'yusancky':
+def PR_TEST():
+    return environ['GITHUB_EVENT_NAME'] == 'pull_request'
+
+def MAIN_REPO_BRANCH():
+    return (environ['GITHUB_REF'] == 'refs/heads/main' and environ['GITHUB_REPOSITORY_OWNER'] == 'yusancky')
+    
+def pull(title : str,split_line = False):
+    if MAIN_REPO_BRANCH():
+        try:
             wiki = Wiki('sat.huijiwiki.com','雨伞CKY',environ['SATWIKI_PASSWORD'])
+            return (wiki.page_text(title).split('\n') if split_line else wiki.page_text(title))
+        except:
+            pass
+    elif PR_TEST():
+        match title:
+            case '模板:天宫空间站任务列表/echarts/data':
+                return [line.replace('\n','') for line in open('test-sources/TSS-data.wikitext')]
+            case _:
+                print('Unable to find test sources.')
+                try:
+                    wiki = Wiki('sat.huijiwiki.com')
+                    return wiki.page_text(title)
+                except:
+                    print(f'You do not have permission to get password.\nREF: {environ["GITHUB_REF"]}\nREPO_OWNER: {environ["GITHUB_REPOSITORY_OWNER"]}')
+    else:
+        try:
+            wiki = Wiki('sat.huijiwiki.com')
             return wiki.page_text(title)
-        else:
-            try:
-                wiki = Wiki('sat.huijiwiki.com')
-                return wiki.page_text(title)
-            except:
-                print(f'You do not have permission to get password.\nREF: {environ["GITHUB_REF"]}\nREPO_OWNER: {environ["GITHUB_REPOSITORY_OWNER"]}')
-    except:
-        pass
+        except:
+            print(f'You do not have permission to get password.\nREF: {environ["GITHUB_REF"]}\nREPO_OWNER: {environ["GITHUB_REPOSITORY_OWNER"]}')
 
 def push(title : str,content_id : str,content : str):
-    print(
-        f'### {content_id}\n\n```go\n{content}\n```\n\n',
-        file = open('PR_preview.md','a',encoding = 'utf-8')
-    )
-    try:
-        if environ['GITHUB_REF'] == 'refs/heads/main' and environ['GITHUB_REPOSITORY_OWNER'] == 'yusancky':
+    if MAIN_REPO_BRANCH():
+        try:
             wiki = Wiki('sat.huijiwiki.com','雨伞CKY',environ['SATWIKI_PASSWORD'])
             wiki.edit(title,content,'Edit via AllUp-Satwiki')
-        else:
-            print(f'You do not have permission to get password.\nREF: {environ["GITHUB_REF"]}\nREPO_OWNER: {environ["GITHUB_REPOSITORY_OWNER"]}')
-    except:
-        pass
+        except:
+            pass
+    elif PR_TEST():
+        print(f'### {content_id}\n\n```go\n{content}\n```\n\n',file = open('PR_preview.md','a'))
+    else:
+        print(f'You do not have permission to get password.\nREF: {environ["GITHUB_REF"]}\nREPO_OWNER: {environ["GITHUB_REPOSITORY_OWNER"]}')
