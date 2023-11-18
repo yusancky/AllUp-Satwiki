90ebb60b115eb7b7d8d0610ae2d0c483fab49b7a
yusancky <yusancky@hotmail.com>
2023-11-18 09:24:09 +0800

    Support to Fetch Starlink Statistics (#69)

--- main.py
+++ main.py
@@ -34,13 +34,21 @@ def make(id):
                 data[section] = findall(f'<td>{section}</td>\n *<td>([^<>]*)</td>',web_data)[0].strip()
             data['#default'] = '请输入正确的选项名！'
             return AllUp_utils.wikitext.build_switch(data)
+        case '3':
+            web_data = AllUp_utils.web.fetch_data('https://planet4589.org/space/con/star/stats.html')
+            data = {'switch_key': 'section'}
+            dataset = findall(r'<TR><TD>Total</TD><TD style="color:blue" *>(\d*)</TD><TD style="color:red" *>\d*</TD><TD style="color:red" *>\d*</TD><TD style="color:red" *>\d*</TD><TD style="color:red" *>\d*</TD><TD style="color:blue" *>(\d*)</TD><TD style="color:blue" *>(\d*)</TD><TD style="color:red" *>\d*</TD><TD style="color:red" *>\d*</TD><TD style="color:red" *>\d*</TD><TD style="color:blue" *>(\d*)</TD><TD style="color:blue" *>\d*</TD><TD style="color:blue" *>\d*</TD><TD style="color:blue" *>\d*</TD><TD style="color:blue" *>\d*</TD><TD style="color:blue" *>\d*</TD><TD style="color:green" *>\d*</TD><TD style="color:green" *>\d*</TD><TD style="color:green" *>\d*</TD><TD style="color:green" *> *</TD><TD style="color:green" *> *</TD><TD style="color:green" *> *</TD></TR>',web_data)
+            for section in range(4):
+                data[str(section + 1)] = dataset[0][section]
+            data['#default'] = '请输入正确的选项名！'
+            return AllUp_utils.wikitext.build_switch(data)
         case _:
             return '请输入正确的AllUp编号！'
 
 if __name__ == '__main__':
     chromedriver = AllUp_utils.web.configure_chromedriver()
     AllUp_data = {'switch_key': '1'}
-    for dataset in ['t'] + [str(i + 1) for i in range(2)] + ['#default']:
+    for dataset in ['t'] + [str(i + 1) for i in range(3)] + ['#default']:
         AllUp_data[dataset] = make(dataset)
     AllUp_content = f'<includeonly>{AllUp_utils.wikitext.build_switch(AllUp_data)}</includeonly><noinclude>[[Category:模板]]{{{{documentation}}}}</noinclude>'
     AllUp_utils.wiki.push('模板:AllUp','MAIN',AllUp_content)
