ed71e4de73a9b193546df08a36e9d52346ff0dce
yusancky <yusancky@hotmail.com>
2024-02-25 15:22:28 +0800

    Update data to GitHub instead of Satwiki

--- AllUp_utils/wiki.py
+++ AllUp_utils/wiki.py
@@ -11,13 +11,7 @@ def MAIN_REPO_BRANCH():
     return (environ['GITHUB_REF'] == 'refs/heads/main' and environ['GITHUB_REPOSITORY_OWNER'] == 'yusancky')
     
 def pull(title : str,split_line = False):
-    if MAIN_REPO_BRANCH():
-        try:
-            wiki = Wiki('sat.huijiwiki.com','雨伞CKY',environ['SATWIKI_PASSWORD'])
-            return (wiki.page_text(title).split('\n') if split_line else wiki.page_text(title))
-        except:
-            pass
-    elif PR_TEST():
+    if MAIN_REPO_BRANCH() or PR_TEST():
         match title:
             case '模板:天宫空间站任务列表/echarts/data':
                 return [line.replace('\n','') for line in open('test-sources/TSS-data.wikitext')]
@@ -36,13 +30,4 @@ def pull(title : str,split_line = False):
             print(f'You do not have permission to get password.\nREF: {environ["GITHUB_REF"]}\nREPO_OWNER: {environ["GITHUB_REPOSITORY_OWNER"]}')
 
 def push(title : str,content_id : str,content : str):
-    if MAIN_REPO_BRANCH():
-        try:
-            wiki = Wiki('sat.huijiwiki.com','雨伞CKY',environ['SATWIKI_PASSWORD'])
-            wiki.edit(title,content,'Edit via AllUp-Satwiki')
-        except:
-            pass
-    elif PR_TEST():
-        print(f'### {content_id}\n\n```go\n{content}\n```\n\n',file = open('PR_preview.md','a'))
-    else:
-        print(f'You do not have permission to get password.\nREF: {environ["GITHUB_REF"]}\nREPO_OWNER: {environ["GITHUB_REPOSITORY_OWNER"]}')
+    open(f'{title}.wikitext', 'w').write(content)
