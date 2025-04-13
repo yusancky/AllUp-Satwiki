5bf18ba1e8809ca1cf6dbc44b9a322b2bac90b47
yusancky <yusancky@hotmail.com>
2025-04-13 16:41:29 +0800

    Add support for processing satellite statistics for multiple satellites (#130)
    Implement data retrieval and processing for a list of 28 different satellites from planet4589.org website

--- main.py
+++ main.py
@@ -128,13 +128,29 @@ def make(id):
   ]
 }}
 |style=min-height:380px}}}}'''
+        case '5':
+            data = {'switch_key': 'sat'}
+            for sat in ['star', 'ow', 'kp', 'stsh', 'xw', 'qf', 'ynh', 'lynk', 'esp', 's1m', 'pln', 'iri', 'gbl', 'jil', 'slog', 'asts', 'swa', 'glo', 'spr', 'st3', 'par', 'gps', 'bei', 'oco', 'hwk', 'kep', 'int', 'ses']:
+                web_data = AllUp_utils.web.fetch_data(f'https://planet4589.org/space/con/{sat}/stats.html')
+                dataInner = {'switch_key': 'section'}
+                dataset = findall(r'<TR><TD>Total</TD><TD style="color:blue" *>(\d*)</TD><TD style="color:red" *>\d*</TD><TD style="color:red" *>\d*</TD><TD style="color:red" *>\d*</TD><TD style="color:red" *>\d*</TD><TD style="color:blue" *>(\d*)</TD><TD style="color:blue" *>(\d*)</TD><TD style="color:red" *>\d*</TD><TD style="color:red" *>\d*</TD><TD style="color:red" *>\d*</TD><TD style="color:blue" *>(\d*)</TD><TD style="color:blue" *>\d*</TD><TD style="color:blue" *>\d*</TD><TD style="color:blue" *>\d*</TD><TD style="color:blue" *>\d*</TD><TD style="color:blue" *>\d*</TD><TD style="color:green" *>\d*</TD><TD style="color:green" *>\d*</TD><TD style="color:green" *>\d*</TD><TD style="color:green" *> *</TD><TD style="color:green" *> *</TD><TD style="color:green" *> *</TD></TR>',web_data)
+                for section in range(4):
+                    try:
+                        dataInner[str(section + 1)] = dataset[0][section]
+                    except Exception as e:
+
+                        dataInner[str(section + 1)] = str(e)
+                dataInner['#default'] = '请输入正确的选项名！'
+                data[sat] = AllUp_utils.wikitext.build_switch(dataInner)
+            data['#default'] = '请输入正确的卫星名！'
+            return AllUp_utils.wikitext.build_switch(data)
         case _:
             return '请输入正确的AllUp编号！'
 
 if __name__ == '__main__':
     chromedriver = AllUp_utils.web.configure_chromedriver()
     AllUp_data = {'switch_key': '1'}
-    for dataset in ['t'] + [str(i + 1) for i in range(4)] + ['#default']:
+    for dataset in ['t'] + [str(i + 1) for i in range(5)] + ['#default']:
         AllUp_data[dataset] = make(dataset)
     AllUp_content = f'<includeonly>{AllUp_utils.wikitext.build_switch(AllUp_data)}</includeonly><noinclude>[[Category:模板]]{{{{documentation}}}}</noinclude>'
     AllUp_utils.wiki.push('AllUp','MAIN',AllUp_content)
