68f1513fb994b2155b92e799c48689ab1157ab43
yusancky <yusancky@hotmail.com>
2025-04-19 10:14:00 +0800

    更新 main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# Copyright (c) yusancky. All rights reserved. 
+# Licensed under the Apache License 2.0. See License in the project root for license information. 
+
 import AllUp_utils.wiki
 import AllUp_utils.web
 import AllUp_utils.wikitext
