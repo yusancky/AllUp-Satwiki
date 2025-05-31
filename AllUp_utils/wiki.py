42b47d410d9e6233325aeb28e3faa94340cf41d0
yusancky <yusancky@hotmail.com>
2025-05-31 20:55:27 +0800

    更新 wiki.py

--- AllUp_utils/wiki.py
+++ AllUp_utils/wiki.py
@@ -1,30 +1,50 @@
-# Copyright (c) yusancky. All rights reserved. 
-# Licensed under the Apache License 2.0. See License in the project root for license information. 
+# Copyright (c) yusancky. All rights reserved.
+# Licensed under the Apache License 2.0. See License in the project root for license information.
 
 from os import environ
 import requests
 from pwiki.wiki import Wiki
 
-def PR_TEST():
-    return environ['GITHUB_EVENT_NAME'] == 'pull_request'
+try:
+    MAIN_REPO_BRANCH = (
+        environ["GITHUB_REPOSITORY_OWNER"] == "yusancky"
+        and environ["GITHUB_REF"] == "refs/heads/main"
+    )
+    TEST_DISPATCH = (
+        environ["GITHUB_REPOSITORY_OWNER"] == "yusancky"
+        and environ["GITHUB_EVENT_NAME"] == "workflow_dispatch"
+    )
+    TEST_PR = environ["GITHUB_EVENT_NAME"] == "pull_request"
+except Exception:
+    MAIN_REPO_BRANCH, TEST_DISPATCH, TEST_PR = False, False, False
 
-def MAIN_REPO_BRANCH():
-    return (environ['GITHUB_REF'] == 'refs/heads/main' and environ['GITHUB_REPOSITORY_OWNER'] == 'yusancky')
-
-# Monkeypatch requests.Session to always include X-authkey
 _original_session_init = requests.Session.__init__
+
 
 def _patched_session_init(self, *args, **kwargs):
     _original_session_init(self, *args, **kwargs)
-    self.headers["X-authkey"] = environ['X_AUTHKEY']
+    self.headers["X-authkey"] = environ["X_AUTHKEY"]
+
 
 requests.Session.__init__ = _patched_session_init
 
-def pull(title : str,split_line = False):
-    wiki = Wiki('sat.huijiwiki.com', '雨伞CKY', environ['BOT_PASSWORD'])
-    return wiki.page_text(title)
+if MAIN_REPO_BRANCH or PR_TEST:
+    wiki = Wiki("sat.huijiwiki.com", "雨伞CKY", environ["BOT_PASSWORD"])
 
-def push(title : str,content_id : str,content : str):
-    open(f'{title}.wikitext', 'w').write(content)
-    if PR_TEST():
-        print(f'### {content_id}\n\n```go\n{content}\n```\n\n',file = open('PR_preview.md','a'))
+
+def pull(title: str, split_line=False):
+    if MAIN_REPO_BRANCH or TEST_DISPATCH or TEST_PR:
+        return wiki.page_text(title)
+    else:
+        return ""
+
+
+def push(title: str, content_id: str, content: str):
+    open(f"{title}.wikitext", "w").write(content)
+    if MAIN_REPO_BRANCH or TEST_DISPATCH:
+        wiki.edit(title, content, "Edit via AllUp-Satwiki")
+    if TEST_PR:
+        print(
+            f"### {content_id}\n\n```go\n{content}\n```\n\n",
+            file=open("PR_preview.md", "a"),
+        )
