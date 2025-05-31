87a544a8d0398b9d5d616108725dc06db0a4e44c
yusancky <yusancky@hotmail.com>
2025-05-31 21:41:16 +0800

    更新 main.py

--- main.py
+++ main.py
@@ -236,4 +236,4 @@ if __name__ == "__main__":
     for dataset in ["t"] + [str(i + 1) for i in range(5)] + ["#default"]:
         AllUp_data[dataset] = make(dataset)
     AllUp_content = f"<includeonly>{AllUp_utils.wikitext.build_switch(AllUp_data)}</includeonly><noinclude>[[Category:模板]]{{{{documentation}}}}</noinclude>"
-    AllUp_utils.wiki.push("AllUp", "MAIN", AllUp_content)
+    AllUp_utils.wiki.push("Template:AllUp", "MAIN", AllUp_content)
