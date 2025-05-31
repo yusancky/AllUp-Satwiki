5439e4aababcdc9b49a9505b71e1b94e0c1da0e0
yusancky <yusancky@hotmail.com>
2025-05-31 20:56:49 +0800

    更新 wiki.py

--- AllUp_utils/wiki.py
+++ AllUp_utils/wiki.py
@@ -2,8 +2,8 @@
 # Licensed under the Apache License 2.0. See License in the project root for license information.
 
 from os import environ
-import requests
 from pwiki.wiki import Wiki
+import requests
 
 try:
     MAIN_REPO_BRANCH = (
