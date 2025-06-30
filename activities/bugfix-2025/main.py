9655df834bc1ed350f26c8ffcc03e7345e5ec926
yusancky <yusancky@hotmail.com>
2025-07-01 01:05:18 +0800

    Update main.py

--- activities/bugfix-2025/main.py
+++ activities/bugfix-2025/main.py
@@ -15,21 +15,15 @@ if __name__ == "__main__":
         username = match[0]
         score = int(match[2])
         user_scores[username] += score
-    if sorted_users := sorted(
-        user_scores.items(), key=lambda x: x[1], reverse=True
-    ):
+    if sorted_users := sorted(user_scores.items(), key=lambda x: x[1], reverse=True):
         current_rank, same_score_count = 1, 1
-        leaderboard += (
-            f"|-\n| '''<center>1</center>''' || [[用户:{sorted_users[0][0]}]] || <font color=\"#D6E\">'''<center>{sorted_users[0][1]}</center>'''</font>\n"
-        )
+        leaderboard += f'|-\n| <center><b>1</b></center> || [[用户:{sorted_users[0][0]}]] || <font color="#D6E"><center><b>{sorted_users[0][1]}</b></center></font>\n'
         for i in range(1, len(sorted_users)):
             if sorted_users[i][1] == sorted_users[i - 1][1]:
                 same_score_count += 1
             else:
                 current_rank += same_score_count
                 same_score_count = 1
-            leaderboard += f"|-\n| '''<center>{current_rank}</center>''' || [[用户:{sorted_users[i][0]}]] || {'<font color=\"#4E4\">' if current_rank <= 3  else ''}'''<center>{sorted_users[i][1]}</center>'''{'</font>' if current_rank <= 5  else ''}\n"
-    leaderboard += (
-        "|-\n| colspan=\"3\" | 统计图表基于[https://github.com/yusancky/AllUp-Satwiki '''AllUp-Satwiki''']生成，于<small>（北京时间）</small>每天13时、22时<small>（可能有一定延迟）</small>自动更新。\n|}"
-    )
+            leaderboard += f"""|-\n| <center><b>{current_rank}</b></center> || [[用户:{sorted_users[i][0]}]] || {'<font color="#4E4">' if current_rank <= 3 else ''}<center>{'<b>' if current_rank <= 5 else ''}{sorted_users[i][1]}{'</b>' if current_rank <= 5 else ''}</center>{'</font>' if current_rank <= 3 else ''}\n"""
+    leaderboard += "|-\n| colspan=\"3\" | 统计图表基于[https://github.com/yusancky/AllUp-Satwiki '''AllUp-Satwiki''']生成，于<small>（北京时间）</small>每天13时、22时<small>（可能有一定延迟）</small>自动更新。\n|}"
     AllUp_utils.wiki.push("Template:天热站破公示/leaderboard", leaderboard)
