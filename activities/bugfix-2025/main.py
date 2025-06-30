77e04fd7eb4bd60776a6673d9c4f390487ef59a0
yusancky <yusancky@hotmail.com>
2025-06-30 16:46:57 +0800

    Update main.py

--- activities/bugfix-2025/main.py
+++ activities/bugfix-2025/main.py
@@ -1,15 +1,15 @@
 # Copyright (c) yusancky. All rights reserved.
 # Licensed under the Apache License 2.0. See License in the project root for license information.
 
-import AllUp_utils.wiki
+from ...AllUp_utils import wiki
 from collections import defaultdict
 from re import findall
 
 if __name__ == "__main__":
     leaderboard = '{| class="wikitable"\n! 排名 !! 用户名 !! 总评分\n'
     pattern = r"\{\{天热站破公示\|1\|([^|]+)\|(\d{1,2}\.\d{1,2})\|[^|]+\|[^|]+\|[^|]+\|(\d{1,2})\}\}"
-    list = AllUp_utils.wiki.pull("博客:天热了，让你站破产吧")
-    matches = findall(pattern, list)
+    pulled_content = AllUp_utils.wiki.pull("博客:天热了，让你站破产吧")
+    matches = findall(pattern, pulled_content)
     user_scores = defaultdict(int)
     for match in matches:
         username = match[0]
@@ -17,16 +17,19 @@ if __name__ == "__main__":
         user_scores[username] += score
     sorted_users = sorted(user_scores.items(), key=lambda x: x[1], reverse=True)
     if sorted_users:
-        current_rank = 1
+        current_rank, same_score_count = 1, 1
         leaderboard += (
             f"|-\n| '''1''' || [[用户:{sorted_users[0][0]}]] || {sorted_users[0][1]}\n"
         )
         for i in range(1, len(sorted_users)):
             if sorted_users[i][1] == sorted_users[i - 1][1]:
-                rank_str = leaderboard[-1].split(",")[0]
+                same_score_count += 1
             else:
-                rank_str = str(i + 1)
-            leaderboard += f"|-\n| '''{rank_str}''' || [[用户:{sorted_users[i][0]}]] || {sorted_users[i][1]}\n"
+                current_rank += same_score_count
+                same_score_count = 1
+            leaderboard += (
+                f"|-\n| '''{current_rank}''' || [[用户:{sorted_users[i][0]}]] || {sorted_users[i][1]}\n"
+            )
     AllUp_utils.wiki.push(
         "Template:天热站破公示/leaderboard", "BugFix-2025-leaderboard", leaderboard
     )
