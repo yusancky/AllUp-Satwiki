c8c728e51fd02be9d41cac090a6b5c74a2b81d7d
yusancky <yusancky@hotmail.com>
2025-07-12 22:59:28 +0800

    Update main.py

--- activities/bugfix-2025/main.py
+++ activities/bugfix-2025/main.py
@@ -17,19 +17,22 @@ RANK_STYLE = {
 
 def show_rank(rank):
     if icon := RANK_EMOJI.get(rank):
-        return icon
-    if rank <= 5:
-        return f"<b>{rank}</b>"
-    return str(rank)
+        return f"<center>{icon}</center>"
+    elif rank <= 5:
+        return f'<font style="font-family: TitilliumWeb-Bold";><b>{rank}</b></font>'
+    else:
+        return f'<font style="font-family: TitilliumWeb-Bold";>{rank}</font>'
 
 
 def show_score(rank, score):
     st = RANK_STYLE.get(rank, {})
     inner = f"<b>{score}</b>" if st.get("bold") else str(score)
-    centered = f"<center>{inner}</center>"
+    formatted = (
+        f'<font style="font-family: TitilliumWeb-Bold";><center>{inner}</center></font>'
+    )
     if color := st.get("color"):
-        return f'<font color="{color}">{centered}</font>'
-    return centered
+        return f'<font color="{color}">{formatted}</font>'
+    return formatted
 
 
 if __name__ == "__main__":
@@ -52,5 +55,5 @@ if __name__ == "__main__":
                 current_rank += same_score_count
                 same_score_count = 1
             leaderboard += f"|-\n| <center>{show_rank(current_rank)}</center> || <center>[[用户:{sorted_users[i][0]}]]</center> || {show_score(current_rank, sorted_users[i][1])}\n"
-    leaderboard += "|-\n| colspan=\"3\" | 排行榜由[https://github.com/yusancky/AllUp-Satwiki '''AllUp''']于<small>（北京时间）</small>每天11时、15时、19时、23时<small>（可能有一定延迟）</small>自动更新。\n|}"
+    leaderboard += '|-\n| colspan="3" | 排行榜由<font style="font-family: TitilliumWeb-Bold";>[https://github.com/yusancky/AllUp-Satwiki AllUp]</font>于<small>（北京时间）</small>每天<font style="font-family: TitilliumWeb-Bold";>11</font>时、<font style="font-family: TitilliumWeb-Bold";>15</font>时、<font style="font-family: TitilliumWeb-Bold";>19</font>时、<font style="font-family: TitilliumWeb-Bold";>23</font>时<small>（可能有一定延迟）</small>自动更新。\n|}'
     AllUp_utils.wiki.push("Template:天热站破公示/leaderboard", leaderboard)
