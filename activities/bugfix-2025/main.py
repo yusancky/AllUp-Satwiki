0bffce37cdc5df1782ddca91fa7acfe509208d37
yusancky <yusancky@hotmail.com>
2025-07-14 22:04:54 +0800

    Lower leaderboard updating frequency (#144)

--- activities/bugfix-2025/main.py
+++ activities/bugfix-2025/main.py
@@ -18,10 +18,9 @@ RANK_STYLE = {
 def show_rank(rank):
     if icon := RANK_EMOJI.get(rank):
         return icon
-    elif rank <= 5:
+    if rank <= 5:
         return f'<font style="font-family: TitilliumWeb-Bold";><b>{rank}</b></font>'
-    else:
-        return f'<font style="font-family: TitilliumWeb-Bold";>{rank}</font>'
+    return f'<font style="font-family: TitilliumWeb-Bold";>{rank}</font>'
 
 
 def show_score(rank, score):
@@ -55,5 +54,5 @@ if __name__ == "__main__":
                 current_rank += same_score_count
                 same_score_count = 1
             leaderboard += f"|-\n| <center>{show_rank(current_rank)}</center> || <center>[[用户:{sorted_users[i][0]}]]</center> || {show_score(current_rank, sorted_users[i][1])}\n"
-    leaderboard += '|-\n| colspan="3" | 排行榜由<font style="font-family: TitilliumWeb-Bold";>[https://github.com/yusancky/AllUp-Satwiki AllUp]</font>于<small>（北京时间）</small>每天<font style="font-family: TitilliumWeb-Bold";>11</font>时、<font style="font-family: TitilliumWeb-Bold";>15</font>时、<font style="font-family: TitilliumWeb-Bold";>19</font>时、<font style="font-family: TitilliumWeb-Bold";>23</font>时<small>（可能有一定延迟）</small>自动更新。\n|}'
+    leaderboard += '|-\n| colspan="3" | 排行榜由<font style="font-family: TitilliumWeb-Bold";>[https://github.com/yusancky/AllUp-Satwiki AllUp]</font>于<small>（北京时间）</small>每天<font style="font-family: TitilliumWeb-Bold";>14</font>时、<font style="font-family: TitilliumWeb-Bold";>23</font>时<small>（可能有一定延迟）</small>自动更新。\n|}'
     AllUp_utils.wiki.push("Template:天热站破公示/leaderboard", leaderboard)
