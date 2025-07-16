3e35a90743b08728de70f57dd4aac1056a31d723
yusancky <yusancky@hotmail.com>
2025-07-16 15:39:15 +0800

    Fix main.py f-string

--- activities/bugfix-2025/main.py
+++ activities/bugfix-2025/main.py
@@ -27,7 +27,9 @@ def show_score(rank, score):
 if __name__ == "__main__":
     leaderboard = '{| class="wikitable" style="background: #FFF"\n! 排名 !! 用户名 !! 总评分\n'
     pattern = r"\{\{天热站破公示\|1\|([^|]+)\|(\d{1,2}\.\d{1,2})\|[^|]+\|[^|]+\|[^|]+\|(\d{1,2})\}\}"
-    pulled_content, revid = AllUp_utils.wiki.pull("博客:天热了，让你站破产吧#公示"), AllUp_utils.wiki.get_last_revid("博客:天热了，让你站破产吧")
+    pulled_content, revid = AllUp_utils.wiki.pull(
+        "博客:天热了，让你站破产吧#公示"
+    ), AllUp_utils.wiki.get_last_revid("博客:天热了，让你站破产吧")
     matches = findall(pattern, pulled_content)
     user_scores = defaultdict(int)
     for match in matches:
@@ -44,5 +46,5 @@ if __name__ == "__main__":
                 current_rank += same_score_count
                 same_score_count = 1
             leaderboard += f"|-\n| <center>{show_rank(current_rank)}</center> || <center>[[用户:{sorted_users[i][0]}]]</center> || {show_score(current_rank, sorted_users[i][1])}\n"
-    leaderboard += '|-\n| colspan="3" | 排行榜由<font style="font-family: formula1-black";>[https://github.com/yusancky/AllUp-Satwiki AllUp]</font>每小时自动获取数据并更新。<small>上次更新：<code>[https://sat.huijiwiki.com/w/index.php?title=博客:天热了，让你站破产吧&oldid={revid} {revid}]</code></small>\n|}'
+    leaderboard += f'|-\n| colspan="3" | 排行榜由<font style="font-family: formula1-black";>[https://github.com/yusancky/AllUp-Satwiki AllUp]</font>每小时自动获取数据并更新。<small>上次更新：<code>[https://sat.huijiwiki.com/w/index.php?title=博客:天热了，让你站破产吧&oldid={revid} {revid}]</code></small>\n|}}'
     AllUp_utils.wiki.push("Template:天热站破公示/leaderboard", leaderboard)
