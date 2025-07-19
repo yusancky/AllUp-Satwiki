878196d8f48ba6d7eea89e49d0396cb6264fd295
yusancky <yusancky@hotmail.com>
2025-07-19 15:19:32 +0800

    Fix `import` order

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # Copyright (c) yusancky. All rights reserved.
 # Licensed under the Apache License 2.0. See License in the project root for license information.
 
-import AllUp_utils.wiki
 import AllUp_utils.web
+import AllUp_utils.wiki
 import AllUp_utils.wikitext
 from datetime import date
 from re import compile, findall
